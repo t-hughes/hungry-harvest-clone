fc99e1dcd5ef585b1f2fda660e69aa897e0e3472
Talon Hughes <talon.hughes@gmail.com>
2016-12-13 11:19:24 -0700

    made more db changes

--- public/js/services/UserCheckoutSrv.js
+++ public/js/services/UserCheckoutSrv.js
@@ -2,12 +2,12 @@ app.service('UserCheckoutSrv', function($http) {
 
     this.addOrder = function(customerDetails, order, orderDetails) {
 
-        return $http({method: 'POST', url: 'http://localhost:2000/api/customers/', data: customerDetails}).then((res) => {
+        return $http({method: 'POST', url: '/api/customers/', data: customerDetails}).then((res) => {
             order.customer_id = res.data.customer_id;
-            $http({method: 'POST', url: 'http://localhost:2000/api/orders', data: order}).then((res) => {
+            $http({method: 'POST', url: '/api/orders', data: order}).then((res) => {
                 orderDetails.forEach((od) => {
                     od.order_id = res.data.order_id;
-                    $http({method: 'POST', url: 'http://localhost:2000/api/order_details', data: od});
+                    $http({method: 'POST', url: '/api/order_details', data: od});
                 });
             });
         });
