e55911f8ab321a56be49fcab6edd06e034178190
Talon Hughes <talon.hughes@gmail.com>
2016-12-13 09:54:30 -0700

    getting db setup for heroku

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const config = require('./config');
 const massive = require('massive');
-const connectionString = config.database;
+const connectionString = process.env.PSQL_DB || config.database;
 const massiveInstance = massive.connectSync({connectionString: connectionString});
 
 module.exports = massiveInstance;
--- public/js/app.js
+++ public/js/app.js
@@ -4,26 +4,38 @@ app.config(function($urlRouterProvider, $stateProvider) {
 
     $urlRouterProvider.otherwise('/home');
 
-    $stateProvider.state('home', {
+    $stateProvider
+
+    .state('home', {
         url: '/home',
         templateUrl: 'views/home.html'
-    }).state('products', {
+    })
+
+    .state('products', {
         url: '/products',
         templateUrl: 'views/products.html',
         controller: 'ProductsCtrl'
-    }).state('productDetails', {
+    })
+
+    .state('productDetails', {
         url: '/productDetails/:id',
         templateUrl: 'views/productDetails.html',
         controller: 'ProductsCtrl'
-    }).state('userCart', {
+    })
+
+    .state('userCart', {
         url: '/userCart',
         templateUrl: 'views/userCart.html',
         controller: 'UserCartCtrl'
-    }).state('userCheckout', {
+    })
+
+    .state('userCheckout', {
         url: '/userCheckout',
         templateUrl: 'views/userCheckout.html',
         controller: 'UserCheckoutCtrl'
-    }).state('about', {
+    })
+
+    .state('about', {
         url: '/about',
         templateUrl: 'views/about.html'
     });
