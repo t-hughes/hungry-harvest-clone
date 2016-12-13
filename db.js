789cc9c30ec9a9725f3523a275d8fcbb58246956
Talon Hughes <talon.hughes@gmail.com>
2016-12-13 16:44:40 -0700

    trying to fix herko so it posts

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const config = require('./config');
 const massive = require('massive');
-const connectionString = process.env.DATABASE_URL || config.database;
+const connectionString = process.env.DATABASE_URL;
 const massiveInstance = massive.connectSync({connectionString: connectionString});
 
 module.exports = massiveInstance;
